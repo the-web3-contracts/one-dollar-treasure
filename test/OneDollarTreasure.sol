a42bf5298bdaccaa66fba470110fefd5c7ced76f
guoshijiang <guoshijiang2012@163.com>
2024-06-24 02:35:52 +0800

    init project

---
+++ test/OneDollarTreasure.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import { OneDollarTreasure } from "../src/Counter.sol";
+
+contract OneDollarTreasureTest is Test {
+
+}
