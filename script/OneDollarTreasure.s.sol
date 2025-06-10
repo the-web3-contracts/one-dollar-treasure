349e845ddb3e6eb9307148b5699f3275855d97b1
seek <guoshijiang2012@163.com>
2025-06-10 15:15:36 +0800

    Update OneDollarTreasure.s.sol

--- script/OneDollarTreasure.s.sol
+++ script/OneDollarTreasure.s.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 import {Script, console} from "forge-std/Script.sol";
-import "../src/contracts/OneDollarTreasure.sol";
+import "../src/OneDollarTreasure.sol";
 import "../src/access/proxy/Proxy.sol";
 
 contract OneDollarTreasureScript is Script {
@@ -17,11 +17,17 @@ contract OneDollarTreasureScript is Script {
         vm.startBroadcast();
         address admin = msg.sender;
 
-        IERC20 tokenAddress = IERC20(address(0xe3b4ECd2EC88026F84cF17fef8bABfD9184C94F0));
+        IERC20 tokenAddress = IERC20(address(0x8A791620dd6260079BF849Dc5567aDC3F2FdC318));
 
         oneDollarTreasure = new OneDollarTreasure();
+
         proxyOneDollarTreasure = new Proxy(address(oneDollarTreasure), address(admin), "");
+
+        console.log("oneDollarTreasure===", address(oneDollarTreasure));
+        console.log("proxyOneDollarTreasure===", address(proxyOneDollarTreasure));
+
         OneDollarTreasure(address(proxyOneDollarTreasure)).initialize(tokenAddress);
+
 
         vm.stopBroadcast();
     }
